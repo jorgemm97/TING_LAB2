ebb288f5ff9ee31a5770f3f3c576bc6c4f71f6fa
Jorge <l3bjorge@gmail.com>
2020-10-17 12:50:43 +0100

    Lab2Heroku

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ vamos a poner todas nuestras paginitas chulas aquí */
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var serviciosRouter = require('./routes/servicios')
+var serviciosRouter = require('./routes/servicios');
+var dbRouter = require('./routes/db');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', serviciosRouter);
+app.use('/', dbRouter );
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/db.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+module.exports = router;
