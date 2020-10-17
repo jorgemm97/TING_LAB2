63a0d4af7ed4da160d610fa6916b2354ef3df9a0
Jorge <l3bjorge@gmail.com>
2020-10-17 10:55:18 +0100

    Lab2Heroku

--- routes/servicios.js
+++ routes/servicios.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Este trozito de javascript solo existe para hacer el GET de la vista servicios.ejs que me he currado en la 
 carpeta de vistas :) */
-router.get('/servicios', function(req, res, next) {
+router.get('/servicios', function(req, res) {
 
 /* Lo único diferente es que aquí hacemos el render de servicios en lugar de index
 El tema de las variables title y demás de momento esta de adorno */
